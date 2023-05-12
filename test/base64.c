720201c67574f419aa1b357e518ddceb09880525
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-05-12 10:59:18 -0400

    feat: rename `base64.c`

--- test/base64.c
+++ test/base64.c
@@ -7,12 +7,14 @@
 
 int main()
 {
+    // 1. Encode
     unsigned char dst[SIZE];
     const size_t dstlen = sizeof(dst);
     size_t *writtenlen = malloc(sizeof(size_t));
     const unsigned char *src = "Hello, World!\n";
     const size_t srclen = strlen(src);
 
+    printf("Encoding\n");
     int result = atchops_base64_encode(dst, dstlen, writtenlen, src, srclen);
     printf("Success? (0 True): %d\n", result);
 
@@ -26,14 +28,14 @@ int main()
         printf("Written Length: %zu\n", *writtenlen);
     }
 
-    printf("\nDecoding\n");
-
+    // 2. Decode
     unsigned char dst2[SIZE];
     const size_t dstlen2 = sizeof(dst2);
     size_t *writtenlen2 = malloc(sizeof(size_t));
     const unsigned char *src2 = dst;
     const size_t srclen2 = *writtenlen;
 
+    printf("\nDecoding\n");
     int result2 = atchops_base64_decode(dst2, dstlen2, writtenlen2, src2, srclen2);
     printf("Success? (0 True): %d\n", result2);
 
