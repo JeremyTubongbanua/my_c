dadc771cee3c2f91b6c06c1986d6e3521b896958
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-05-12 10:48:21 -0400

    feat: working base64 encode/decode

--- src/base64.c
+++ src/base64.c
@@ -10,3 +10,13 @@ int atchops_base64_encode(unsigned char *dst, size_t dstlen, size_t *writtenlen,
         src,
         srclen);
 }
+
+int atchops_base64_decode(unsigned char *dst, size_t dstlen, size_t *writtenlen, const unsigned char *src, const size_t srclen)
+{
+    return mbedtls_base64_decode(
+        dst,
+        dstlen,
+        writtenlen,
+        src,
+        srclen);
+}
--- test/main.c
+++ test/main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "base64.h"
 
-#define SIZE 10000
+#define SIZE 100
 
 int main()
 {
     unsigned char dst[SIZE];
     const size_t dstlen = sizeof(dst);
-    size_t *writtenlen;
-    unsigned char *src = "Hello, World!\n";
+    size_t *writtenlen = malloc(sizeof(size_t));
+    const unsigned char *src = "Hello, World!\n";
     const size_t srclen = strlen(src);
 
     int result = atchops_base64_encode(dst, dstlen, writtenlen, src, srclen);
     printf("Success? (0 True): %d\n", result);
-    
+
     if (result == 0)
     {
 
@@ -24,5 +25,30 @@ int main()
         printf("Destination Length: %zu\n", dstlen);
         printf("Written Length: %zu\n", *writtenlen);
     }
+
+    printf("\nDecoding\n");
+
+    unsigned char dst2[SIZE];
+    const size_t dstlen2 = sizeof(dst2);
+    size_t *writtenlen2 = malloc(sizeof(size_t));
+    const unsigned char *src2 = dst;
+    const size_t srclen2 = *writtenlen;
+
+    int result2 = atchops_base64_decode(dst2, dstlen2, writtenlen2, src2, srclen2);
+    printf("Success? (0 True): %d\n", result2);
+
+    if (result2 == 0)
+    {
+
+        printf("Source: %s\n", src2);
+        printf("Source Length: %zu\n", srclen2);
+        printf("Destination: %s\n", dst2);
+        printf("Destination Length: %zu\n", dstlen2);
+        printf("Written Length: %zu\n", *writtenlen2);
+    }
+
+    free(writtenlen);
+    free(writtenlen2);
+
     return 0;
 }
