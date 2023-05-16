6ca0cf7ca2150d8eebb6200b86bed0478cbde6b5
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-05-16 17:58:54 -0400

    feat: aes 256 ctr decrypt

--- src/aes256.c
+++ src/aes256.c
@@ -5,29 +5,34 @@
 #include "base64.h"
 
 #define AES_KEY_BITS 256
-#define IV_SIZE 16
+#define AES_KEY_BYTES AES_KEY_BITS / 8
+#define IV_AMOUNT_BYTES 16
+
+#define MAX_BYTES_ALLOCATED_FOR_ENCRYPTION_OPERATION 5000
+#define MAX_TEXT_LENGTH_FORBASE64_ENCODING_OPERATION 10000 // the max length of base64 encoded text (should be more than enough)
 
 AESResult *atchops_aes256_encrypt(const char *key_base64, const unsigned char *plaintext)
 {
-    size_t len = strlen(plaintext);
+    size_t plaintext_len = strlen(plaintext);
+
     // pad the plain text to be a multiple of 16 bytes
     // printf("Padding...\n");
     unsigned char *plaintext_padded;
     size_t plaintext_paddedlen;
 
-    const short int mod = len % 16;
+    const short int mod = plaintext_len % 16;
     const short int num_pad_bytes_to_add = 16 - mod;
     const unsigned char padding_val = num_pad_bytes_to_add;
 
-    plaintext_paddedlen = len + num_pad_bytes_to_add;
+    plaintext_paddedlen = plaintext_len + num_pad_bytes_to_add;
     // printf("plaintext_paddedlen: %lu = %d + %d\n", plaintext_paddedlen, len, num_pad_bytes_to_add);
-    plaintext_padded = malloc(sizeof(unsigned char) * (plaintext_paddedlen+1));
-    for(int i = 0; i < len; i++)
+    plaintext_padded = malloc(sizeof(unsigned char) * (plaintext_paddedlen + 1));
+    for (int i = 0; i < plaintext_len; i++)
     {
         // printf("ia: %d\n", i);
         *(plaintext_padded + i) = *(plaintext + i);
     }
-    for(int i = len; i < len + num_pad_bytes_to_add; i++)
+    for (int i = plaintext_len; i < plaintext_len + num_pad_bytes_to_add; i++)
     {
         *(plaintext_padded + i) = padding_val;
         // printf("ib: %d\n", i);
@@ -36,7 +41,7 @@ AESResult *atchops_aes256_encrypt(const char *key_base64, const unsigned char *p
     // printf("Plaintext Padded: \"%s\"\n", plaintext_padded);
 
     // initialize AES key
-    unsigned char key[32];
+    unsigned char key[AES_KEY_BYTES];
     size_t keylen = sizeof(key);
     size_t *writtenlen = malloc(sizeof(size_t));
 
@@ -48,10 +53,9 @@ AESResult *atchops_aes256_encrypt(const char *key_base64, const unsigned char *p
     mbedtls_aes_setkey_enc(ctx, key, AES_KEY_BITS);
 
     size_t *iv_ctr = malloc(sizeof(unsigned int));
-    unsigned char *iv = malloc(sizeof(unsigned char) * IV_SIZE);
-    unsigned char *stream_block = malloc(sizeof(unsigned char) * IV_SIZE);
-
-    unsigned char *aes_encrypted = malloc(sizeof(unsigned char) * 2048);
+    unsigned char *iv = malloc(sizeof(unsigned char) * IV_AMOUNT_BYTES);
+    unsigned char *stream_block = malloc(sizeof(unsigned char) * IV_AMOUNT_BYTES);
+    unsigned char *aes_encrypted = malloc(sizeof(unsigned char) * MAX_BYTES_ALLOCATED_FOR_ENCRYPTION_OPERATION);
 
     // run encrypt
     mbedtls_aes_crypt_ctr(ctx, plaintext_paddedlen, iv_ctr, iv, stream_block, plaintext_padded, aes_encrypted);
@@ -59,14 +63,15 @@ AESResult *atchops_aes256_encrypt(const char *key_base64, const unsigned char *p
     // find how much of the encrypted data is actually used
     int aes_encryptedlen = 0;
     unsigned char byte;
-    do {
+    do
+    {
         byte = *(aes_encrypted + aes_encryptedlen++);
         // printf("%x\n", byte);
-    } while(byte != 0);
+    } while (byte != 0);
     --aes_encryptedlen;
 
     // encode the encrypted data in base64
-    size_t dstlen = 2048;
+    size_t dstlen = MAX_TEXT_LENGTH_FORBASE64_ENCODING_OPERATION;
     unsigned char *dst = malloc(sizeof(unsigned char) * dstlen);
     atchops_base64_encode(dst, dstlen, writtenlen, aes_encrypted, aes_encryptedlen);
 
@@ -77,7 +82,7 @@ AESResult *atchops_aes256_encrypt(const char *key_base64, const unsigned char *p
     aes_result->res = dst;
     aes_result->reslen = *writtenlen;
 
-    mbedtls_aes_free(ctx);    
+    mbedtls_aes_free(ctx);
     free(iv_ctr);
     free(iv);
     free(stream_block);
@@ -86,8 +91,63 @@ AESResult *atchops_aes256_encrypt(const char *key_base64, const unsigned char *p
     return aes_result;
 }
 
-AESResult *atchops_aes256_decrypt(const char *key, const unsigned char *plaintext)
+AESResult *atchops_aes256_decrypt(const char *key_base64, const unsigned char *ciphertext)
 {
     AESResult *result = malloc(sizeof(AESResult));
+
+    // initialize AES key
+
+    unsigned char key[AES_KEY_BYTES];
+    size_t keylen = sizeof(key);
+
+    size_t *writtenlen = malloc(sizeof(size_t));
+    atchops_base64_decode(key, keylen, writtenlen, key_base64, strlen(key_base64));
+
+    // initialize AES context
+
+    mbedtls_aes_context *ctx = malloc(sizeof(mbedtls_aes_context));
+    mbedtls_aes_init(ctx);
+    mbedtls_aes_setkey_enc(ctx, key, AES_KEY_BITS);
+
+    // decode the base64 ciphertext
+    size_t dstlen = MAX_TEXT_LENGTH_FORBASE64_ENCODING_OPERATION;
+    unsigned char *dst = malloc(sizeof(unsigned char) * dstlen);
+    atchops_base64_decode(dst, dstlen, writtenlen, ciphertext, strlen(ciphertext));
+
+    // run decrypt
+    size_t *iv_ctr = malloc(sizeof(unsigned int));
+    unsigned char *iv = malloc(sizeof(unsigned char) * IV_AMOUNT_BYTES);
+    unsigned char *stream_block = malloc(sizeof(unsigned char) * IV_AMOUNT_BYTES);
+    unsigned char *aes_decrypted = malloc(sizeof(unsigned char) * MAX_BYTES_ALLOCATED_FOR_ENCRYPTION_OPERATION);
+
+    mbedtls_aes_crypt_ctr(ctx, *writtenlen, iv_ctr, iv, stream_block, dst, aes_decrypted);
+
+    // find how much of the decrypted data is actually used
+    int aes_decryptedlen = 0;
+    unsigned char byte;
+    do
+    {
+        byte = *(aes_decrypted + aes_decryptedlen++);
+        // printf("%x\n", byte);
+    } while (byte != 0);
+
+    // remove padding
+    aes_decryptedlen -= 2;
+    unsigned char *aes_decrypted_unpadded = malloc(sizeof(unsigned char) * aes_decryptedlen);
+    for (int i = 0; i < aes_decryptedlen; i++)
+    {
+        *(aes_decrypted_unpadded + i) = *(aes_decrypted + i);
+    }
+
+    // done
+    result->res = aes_decrypted_unpadded;
+    result->reslen = aes_decryptedlen;
+
+    mbedtls_aes_free(ctx);
+    free(iv_ctr);
+    free(iv);
+    free(stream_block);
+    free(aes_decrypted);
+
     return result;
 }
--- test/test_aes256.c
+++ test/test_aes256.c
@@ -4,9 +4,13 @@
 
 int main()
 {
-    const unsigned char *plaintext = "lemonade";
+    const unsigned char *plaintext = "lemonade++/123! Hello, World!\n";
     const unsigned char *key = "1DPU9OP3CYvamnVBMwGgL7fm8yB1klAap0Uc5Z9R79g="; // AES 256 Key (encoded in base64)
     AESResult *result = atchops_aes256_encrypt(key, plaintext);
+    printf("Result: %s\n", result->res);
+    printf("Result length: %lu\n", result->reslen);
+
+    result = atchops_aes256_decrypt(key, result->res);
     printf("Result: %s\n", result->res);
     printf("Result length: %lu\n", result->reslen);
     return 0;    
